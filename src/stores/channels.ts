b39612a0224eac193324688bb9250e671e383c3f
Csető Samuel <Samuel.Cseto@ysoft.com>
2024-10-19 01:41:46 +0200

    feat: add messages, input, profile and etc

--- src/stores/channels.ts
+++ src/stores/channels.ts
@@ -5,78 +5,63 @@ export const useChannelsStore = defineStore('channels', () => {
   const channels = ref([
     {
       id: 1,
-      name: 'First Channel',
-      avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
+      name: 'Chill Vibes',
+      private: true,
+      new: false
     },
     {
       id: 2,
-      name: 'Second Channel',
-      avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
+      name: 'Fitness Hub',
+      private: true,
+      new: false
     },
     {
       id: 3,
-      name: 'Third Channel',
-      avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
+      name: "Gamer's Paradise",
+      private: false,
+      new: false
     },
     {
       id: 4,
-      name: 'Fourth Channel',
-      avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
+      name: 'Art & Crafts Collective',
+      private: false,
+      new: false
     },
     {
       id: 5,
-      name: 'Fifth Channel',
-      avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
+      name: 'Tech Savvy',
+      private: true,
+      new: true
     },
     {
       id: 6,
-      name: 'Sixth Channel',
-      avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
+      name: 'Health and Harmony',
+      private: false,
+      new: false
     },
     {
       id: 7,
-      name: 'Seventh Channel',
-      avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
+      name: 'Music Junkies',
+      private: false,
+      new: false
     },
     {
       id: 8,
-      name: 'Eighth Channel',
-      avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
+      name: 'The Coding Den',
+      private: true,
+      new: false
     },
     {
       id: 9,
-      name: 'Ninth Channel',
-      avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
+      name: 'Creative Space',
+      private: true,
+      new: false
     },
     {
       id: 10,
-      name: 'Tenth Channel',
-      avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
-    },
-    {
-      id: 11,
-      name: 'Eleventh Channel',
-      avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
-    },
-    {
-      id: 12,
-      name: 'Twelfth Channel',
-      avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
-    },
-    {
-      id: 13,
-      name: 'Thirteenth Channel',
-      avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
-    },
-    {
-      id: 14,
-      name: 'Fourteenth Channel',
-      avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
-    },
-    {
-      id: 15,
-      name: 'Fifteenth Channel',
-      avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
+      name: 'Foodies United',
+      private: false,
+      new: false
     }
   ])
 
